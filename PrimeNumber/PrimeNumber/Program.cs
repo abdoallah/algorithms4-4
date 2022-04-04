3e6e0993e22ec4e98725d0c06842b34db8f90376
abdallah <abdallah.shahene@gmail.com>
2022-04-04 13:26:13 +0200

    Add files via upload

---
+++ PrimeNumber/PrimeNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PrimeNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter number");
+            int Number;
+            Number = int.Parse(Console.ReadLine());
+            int d = 0;
+            for(int i=1; i<=Number;i++)
+            {
+                if (Number % i == 0)
+                {
+                    d++;
+                }
+               
+            } 
+            if(d==2)
+                    Console.WriteLine("prime");
+                else
+                    Console.WriteLine("not prime");
+        }
+    }
+}
